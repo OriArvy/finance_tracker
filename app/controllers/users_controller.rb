1504f2c4efe0914ca8a7abceeea115eb02a5dd74
Rokas Vaitkevicius <rokas.vaitkevicius1@gmail.com>
2021-05-27 23:23:12 +0300

    Add functioning user follow/unfollow interaction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,28 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
-  
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      # excluding current user
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid user to search for"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a user to search for"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
+
 end
